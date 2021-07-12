0d204fee0e9fab5c26d902806b968d32d1e04d1f
udaysree <kuday.sree303@gmail.com>
2021-07-12 15:01:44 +0530

    slider blade created

---
+++ app/Http/Controllers/BasicSettings/SliderController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\BasicSettings;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SliderController extends Controller
+{
+    //
+
+    $img = $request->file('file');
+        $allowedExts = array('jpg', 'png', 'jpeg');
+
+        $rules = [
+            'file' => [
+                function ($attribute, $value, $fail) use ($img, $allowedExts) {
+                    if (!empty($img)) {
+                        $ext = $img->getClientOriginalExtension();
+                        if (!in_array($ext, $allowedExts)) {
+                            return $fail("Only png, jpg, jpeg image is allowed");
+                        }
+                    }
+                },
+            ],
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $validator->getMessageBag()->add('error', 'true');
+            return response()->json(['errors' => $validator->errors(), 'id' => 'slider']);
+        }
+
+        $filename = time() . '.' . $img->getClientOriginalExtension();
+        $request->session()->put('slider_image', $filename);
+        $request->file('file')->move('assets/front/img/sliders/', $filename);
+        return response()->json(['status' => "session_put", "image" => "slider_image", 'filename' => $filename]);
+}
---
+++ database/migrations/2021_07_12_055657_create_sliders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255)->nullable();
+            $table->string('text', 255)->nullable();
+            $table->string('button_text', 255)->nullable();
+            $table->string('button_url', 255)->nullable();
+            $table->string('image', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+}
